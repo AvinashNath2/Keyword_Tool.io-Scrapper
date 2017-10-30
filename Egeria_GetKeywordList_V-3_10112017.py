e0b226a655ab11b924d51a06c892c9cef052adc4
Avinash Nath <avinashnath2@gmail.com>
2017-10-30 18:32:31 +0530

    Add files via upload

---
+++ Egeria_GetKeywordList_V-3_10112017.py
@@ -0,0 +1,109 @@
+import os
+from selenium import webdriver
+
+from bs4 import BeautifulSoup
+import csv
+from time import sleep
+from io import StringIO
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+#########################################  Crome Drivers {Setup}  ##########################################
+#Download Crome Web Drivers {https://chromedriver.storage.googleapis.com/index.html?path=2.33/}
+
+#Set the PATH of Crome Driver 
+chromedriver = "C:/Sele_Drivers/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+
+#Loading Site {Log In page}
+driver.get("https://keywordtool.io/user?destination=node")
+#Variable to store Password and ID
+KT_user_name = "pramod@scalelabs.in"
+KT_user_pass = "scalelabs2016"
+#ID's of both email and password TextBox
+emailFieldID = "edit-name"
+passFieldID  = "edit-pass"
+#Xpath for Login Button 
+loginButtonXpath = "//button[@value='Log in']"
+#Xpath for Search Button 
+searchButtonXpath = "//button[@type='submit']"
+#Xpath for Downloading CSV 
+downloadingButtonXpath = "(//*[contains(text(),'CSV')])[0]"
+#ID of SearchBox
+searchBoxID = "edit-keyword" 
+#searching the variable
+KT_search_string  = ""
+
+#Main Working Tab of the web Browser
+main_window = driver.current_window_handle
+
+#finding Text View by ID
+emailFieldElement  = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_id(emailFieldID))
+passfieldElement   = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_id(passFieldID))
+
+#finding by Xpath
+loginButtonElement = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_xpath(loginButtonXpath))
+
+#Clearing Saved UserID/Password
+emailFieldElement.clear()
+passfieldElement.clear()
+
+#Setting UserID/Password 
+emailFieldElement.send_keys(KT_user_name)
+passfieldElement.send_keys(KT_user_pass)
+
+#Clicking the Login Button
+loginButtonElement.click()
+#Search Button 
+searchButtonElement = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_xpath(searchButtonXpath))
+#Input Text in Search TextView
+searchFieldElement = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_id(searchBoxID))
+
+#Setting the Cursor to the list of Google Server
+searchFieldElement.send_keys(Keys.TAB)
+select_Drop_down = driver.switch_to.active_element
+select_Drop_down.send_keys(Keys.RETURN)
+
+#Setting The Google Server by putting it's position from the list view
+position = 78
+for i in range(position):
+    select_Drop_down.send_keys(Keys.DOWN)
+select_Drop_down.send_keys(Keys.RETURN)
+
+#Reading CSV file
+with open('test.csv','r') as csvfile: 
+    reader = csv.reader(csvfile)
+    for row in reader:
+        #Reading line by line from the csv file 
+        searchFieldElement = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_id(searchBoxID))
+        KT_search_string = row[0]
+        #Input Text in Search TextView
+        searchFieldElement.send_keys(KT_search_string)
+        searchButtonElement = WebDriverWait(driver,10).until(lambda driver: driver.find_element_by_xpath(searchButtonXpath))
+        #Click on the search Button
+        searchButtonElement.click()
+        #Printing searched element
+        print("Search success -> " + KT_search_string)
+
+        sleep(5)
+        
+        #To get the CSV Downloading From Drop down menu on the Left
+        actions = ActionChains(driver) 
+        actions.send_keys(Keys.TAB * 13)
+        actions.send_keys(Keys.RETURN)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.RETURN)
+        actions.perform()   
+        #Sleep in order to get the load full page
+        sleep(7)
+        #Switching from Downloading window to Main Window
+        driver.switch_to_window(main_window)
+        #Loading Home location for next search
+        driver.get("https://keywordtool.io")
+
+#Close Web Driver{Crome}
+driver.quit()
